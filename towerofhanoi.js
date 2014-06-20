8f72c2998c67fabb0db83dc823b800c7b91776a5
beatrizrezener <beatrizrezener@gmail.com>
2014-06-20 15:22:19 -0300

    Adding example elements in the scene

--- towerofhanoi.js
+++ towerofhanoi.js
@@ -1,75 +1,63 @@
 //set main namespace
 goog.provide('towerofhanoi');
 
-
 //get requirements
 goog.require('lime.Director');
 goog.require('lime.Scene');
-goog.require('lime.Layer');
-goog.require('lime.Circle');
-goog.require('lime.Label');
-goog.require('lime.animation.Spawn');
-goog.require('lime.animation.FadeTo');
-goog.require('lime.animation.ScaleTo');
+goog.require('lime.Sprite');
+goog.require('lime.fill.LinearGradient'); //Classe inclu�da para criar o gradiente do c�u
+goog.require('lime.Polygon');
 goog.require('lime.animation.MoveTo');
-
+goog.require('lime.animation.RotateBy');
+goog.require('lime.animation.FadeTo');
+goog.require('lime.transitions.SlideInRight');
+goog.require('lime.transitions.Dissolve');
 
 // entrypoint
-towerofhanoi.start = function(){
+towerofhanoi.start = function() {
 
-	var director = new lime.Director(document.body,1024,768),
-	    scene = new lime.Scene(),
+    var director = new lime.Director(document.body, 800, 640);
+    director.makeMobileWebAppCapable();
+    director.setDisplayFPS(false);
 
-	    target = new lime.Layer().setPosition(512,384),
-        circle = new lime.Circle().setSize(150,150).setFill(255,150,0),
-        lbl = new lime.Label().setSize(160,50).setFontSize(30).setText('TOUCH ME!'),
-        title = new lime.Label().setSize(800,70).setFontSize(60).setText('Now move me around!')
-            .setOpacity(0).setPosition(512,80).setFontColor('#999').setFill(200,100,0,.1);
+    var scene1 = new lime.Scene();
 
+    /* fundo */
+    //Definindo um gradiente linear, para o c�u azul gradiente, que neste caso come�a
+    //no topo esquerdo da tela e termina na direita em baixo. Come�a no (0,0) e termina no (1,1)
+    var sky_gradient = new lime.fill.LinearGradient().setDirection(0, 0, 1, 1)
+            .addColorStop(0, '#B2DFEE').addColorStop(1, '#0000CD'); //#0000CD = dark blue
 
-    //add circle and label to target object
-    target.appendChild(circle);
-    target.appendChild(lbl);
+    var sky = new lime.Sprite().setSize(800,640).setPosition(0,0).setAnchorPoint(0,0).setFill(sky_gradient);
 
-    //add target and title to the scene
-    scene.appendChild(target);
-    scene.appendChild(title);
-
-	director.makeMobileWebAppCapable();
-
-    //add some interaction
-    goog.events.listen(target,['mousedown','touchstart'],function(e){
-
-        //animate
-        target.runAction(new lime.animation.Spawn(
-            new lime.animation.FadeTo(.5).setDuration(.2),
-            new lime.animation.ScaleTo(1.5).setDuration(.8)
-        ));
-
-        title.runAction(new lime.animation.FadeTo(1));
-
-        //let target follow the mouse/finger
-        e.startDrag();
-
-        //listen for end event
-        e.swallow(['mouseup','touchend'],function(){
-            target.runAction(new lime.animation.Spawn(
-                new lime.animation.FadeTo(1),
-                new lime.animation.ScaleTo(1),
-                new lime.animation.MoveTo(512,384)
-            ));
-
-            title.runAction(new lime.animation.FadeTo(0));
-        });
-
-
-    });
-
-	// set current scene active
-	director.replaceScene(scene);
-
-}
-
-
-//this is required for outside access after code is compiled in ADVANCED_COMPILATIONS mode
-goog.exportSymbol('towerofhanoi.start', towerofhanoi.start);
+    /* FOOTER */
+    //setFill('rgb(0,128,0)') OU setFill(0,128,0); podemos setar a cor como HEXADECIMAL e como RGB.
+    var footer = new lime.Sprite().setSize(800,40).setPosition(0,600).setAnchorPoint(0,0).setFill('rgb(0,0,0)');
+    
+    /* PLATAFORM*/
+    var plataform = new lime.Polygon().setPosition(100,560).setAnchorPoint(0,0).setFill('assets/texturamadeira.jpg').addPoints(-40,40,0,0, 600,0, 640,40,-40,40);
+    
+    /* BASES */
+    var base1 = new lime.Sprite().setSize(20,140).setPosition(170,420).setAnchorPoint(0,0).setFill('assets/texturamadeiraclara2.jpg');
+    var base2 = new lime.Sprite().setSize(20,140).setPosition(390,420).setAnchorPoint(0,0).setFill('assets/texturamadeiraclara2.jpg');
+    var base3 = new lime.Sprite().setSize(20,140).setPosition(610,420).setAnchorPoint(0,0).setFill('assets/texturamadeiraclara2.jpg');
+    
+    /* DISCS */
+    var disc1 = new lime.Sprite().setSize(60,20).setPosition(150,500).setAnchorPoint(0,0).setFill('assets/madeiraazul4.jpg');
+    var disc2 = new lime.Sprite().setSize(80,20).setPosition(140,520).setAnchorPoint(0,0).setFill('assets/madeiravermelha.jpg');
+    var disc3 = new lime.Sprite().setSize(100,20).setPosition(130,540).setAnchorPoint(0,0).setFill('assets/madeiraverde2.jpg');
+    
+    //add os elementos na cena
+    scene1.appendChild(sky);
+    scene1.appendChild(footer);
+    scene1.appendChild(plataform);
+    scene1.appendChild(base1);
+    scene1.appendChild(base2);
+    scene1.appendChild(base3);
+    scene1.appendChild(disc1);
+    scene1.appendChild(disc2);
+    scene1.appendChild(disc3);
+    
+    // set current scene active
+    director.replaceScene(scene1);
+};
