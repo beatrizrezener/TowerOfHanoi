a2e53c5af9b2f03bc2536183d1696ad402605707
carol15022 <carol15022@hotmail.com>
2014-06-25 22:51:46 -0300

    PauseScene

--- pausescene.js
+++ pausescene.js
@@ -15,8 +15,11 @@ towerofhanoi.PauseScene = function() {
     
     this.domElement.style.cssText = 'background:rgba(255,255,255,.8)';
 
-    var label = new lime.Label().setText('PAUSE').setPosition(400,300).setFontColor('#B6767F').setFontSize(50);
+    var label = new lime.Label().setText('PAUSE').setPosition(400,50).setFontColor('#B6767F').setFontSize(50);
     layer.appendChild(label);
+
+    var instructions = new lime.Sprite().setSize(600,300).setPosition(100,170).setAnchorPoint(0,0).setFill('assets/instructions2.png');
+    layer.appendChild(instructions);    
     
     var btn_play = new lime.Sprite().setSize(75,75).setPosition(689,29).setAnchorPoint(0,0).setFill('assets/play.png');
     layer.appendChild(btn_play);
