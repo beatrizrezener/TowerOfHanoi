274ba801f93b6fb2e2ad1536ea2012d26ff83289
David Carlos <ddavidcarlos1392@gmail.com>
2014-06-28 11:24:18 -0300

    Separating winner design from winner logic.

---
+++ winner_design.js
@@ -0,0 +1,84 @@
+goog.provide("towerofhanoi.winner_design");
+ towerofhanoi.winner_design = function winner_design(number_of_moviments,n_disks){
+
+
+      var scene = new lime.Scene();
+
+      var layer = new lime.Layer();
+
+      towerofhanoi.play_sound_win();
+
+      number_of_moviments += 1;
+      score(layer, number_of_moviments, n_disks);
+      
+      var continue_play = new towerofhanoi.Button("PLAY AGAIN")
+        .setSize(200, 100)
+        .setPosition(250, 360);
+ 
+      var next_level = new towerofhanoi.Button("NEXT LEVEL")
+        .setSize(200, 100)
+        .setPosition(550, 360);
+
+      var back_to_menu = new towerofhanoi.Button("Menu")
+        .setSize(170, 70)
+        .setPosition(140, 590);
+
+      var maskSprite = new lime.Sprite()
+        .setSize(800, 640).setFill(100, 0, 0, .1)
+        .setAnchorPoint(0, 0);
+         
+      scene.appendChild(maskSprite);
+      layer.appendChild(continue_play);
+      layer.appendChild(back_to_menu);
+      layer.appendChild(next_level);
+      scene.appendChild(layer);
+
+      towerofhanoi.director.replaceScene(scene);
+      goog.events.listen(continue_play, ['mousedown', 'touchstart'], function(e) {
+        towerofhanoi.Game.playAgain(n_disks);
+      });
+
+      goog.events.listen(next_level, ['mousedown', 'touchstart'], function(e) {
+        if(n_disks === 7) {
+          alert("This is the last level.");
+        } else {
+          towerofhanoi.Game.playAgain(n_disks+1);
+        }
+      });
+
+      goog.events.listen(back_to_menu, ['mousedown', 'touchstart'], function(e) {
+        towerofhanoi.loadMenu();
+        towerofhanoi.Game.resetMoviments();
+      });
+    }
+
+function score(layer, number_of_moviments, qtyDiscs) {
+    var number_moviments_three_stars = (Math.pow(2, qtyDiscs)) - 1
+    var number_moviments_two_stars = ((Math.pow(2, qtyDiscs)) - 1) + (((Math.pow(2, qtyDiscs)) - 1)/2)
+    var number_moviments_one_star = ((Math.pow(2, qtyDiscs)) - 1) + ((Math.pow(2, qtyDiscs)) - 1)
+
+    if(number_of_moviments >= number_moviments_three_stars && number_of_moviments < number_moviments_two_stars){
+        var three_stars = new lime.Sprite()
+            .setSize(512, 256)
+            .setPosition(135, 60)
+            .setAnchorPoint(0, 0)
+            .setFill('assets/three_stars.png');
+        layer.appendChild(three_stars);
+    }
+    if(number_of_moviments >= number_moviments_two_stars && number_of_moviments < number_moviments_one_star){
+        var two_stars = new lime.Sprite()
+            .setSize(512, 256)
+            .setPosition(135, 60)
+            .setAnchorPoint(0, 0)
+            .setFill('assets/two_stars.png');
+        layer.appendChild(two_stars);
+    }
+    if(number_of_moviments >= number_moviments_one_star){
+        var one_star = new lime.Sprite()
+            .setSize(512, 256)
+            .setPosition(135, 60)
+            .setAnchorPoint(0, 0)
+            .setFill('assets/one_star.png');
+        layer.appendChild(one_star);
+    }
+}
