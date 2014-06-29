7b2343432a63f5466efac6bd136bd3a645920cfa
beatrizrezener <beatrizrezener@gmail.com>
2014-06-28 23:02:00 -0300

    Adjusting coord. gameover picture. Removing back to menu from pause screen.

--- end_game_pro.js
+++ end_game_pro.js
@@ -1,28 +1,31 @@
 goog.provide("towerofhanoi.end_game_pro");
 
-towerofhanoi.end_game_pro = function(n_disks){
+towerofhanoi.end_game_pro = function(n_disks) {
     var scene = new lime.Scene();
     var layer = new lime.Layer();
 
     towerofhanoi.play_sound_lost();
 
+    maskSprite = new lime.Sprite().setSize(800, 640).setFill(100, 0, 0, .1).setAnchorPoint(0, 0);
+    layer.appendChild(maskSprite);
+
     var loose = new lime.Sprite()
             .setSize(512, 150)
-            .setPosition(100, 100)
+            .setPosition(150, 100)
             .setAnchorPoint(0, 0)
             .setFill('assets/gameover.png');
-  
+
     var continue_play = new towerofhanoi.Button("PLAY AGAIN")
-      .setSize(200, 100)
-      .setPosition(365, 400);
+            .setSize(200, 100)
+            .setPosition(400, 400);
 
     var back_to_menu = new towerofhanoi.Button("Menu")
-      .setSize(170, 70)
-      .setPosition(140, 590);
+            .setSize(170, 70)
+            .setPosition(140, 590);
 
     var maskSprite = new lime.Sprite()
-      .setSize(800, 640).setFill(100, 0, 0, .1)
-      .setAnchorPoint(0, 0);
+            .setSize(800, 640).setFill(100, 0, 0, .1)
+            .setAnchorPoint(0, 0);
 
     layer.appendChild(continue_play);
     layer.appendChild(back_to_menu);
@@ -31,12 +34,12 @@ towerofhanoi.end_game_pro = function(n_disks){
 
     towerofhanoi.director.replaceScene(scene);
     goog.events.listen(continue_play, ['mousedown', 'touchstart'], function(e) {
-      towerofhanoi.Game.playAgain(n_disks);
+        towerofhanoi.Game.playAgain(n_disks);
     });
 
     goog.events.listen(back_to_menu, ['mousedown', 'touchstart'], function(e) {
-      towerofhanoi.loadMenu();
-      towerofhanoi.Game.resetMoviments();
+        towerofhanoi.loadMenu();
+        towerofhanoi.Game.resetMoviments();
     });
 
-    }
+};
--- pausescene.js
+++ pausescene.js
@@ -22,19 +22,8 @@ towerofhanoi.PauseScene = function() {
     var instructions = new lime.Sprite().setSize(600,300).setPosition(100,170).setAnchorPoint(0,0).setFill('assets/instructions.png');
     layer.appendChild(instructions);    
     
-    var btn_play = new towerofhanoi.Button("RESUME GAME").setSize(220,50).setPosition(585,500).setAnchorPoint(0,0);
+    var btn_play = new towerofhanoi.Button("RESUME GAME").setSize(220,50).setPosition(400,500).setAnchorPoint(0,0);
     layer.appendChild(btn_play);
-    
-    var btn_backtomenu = new towerofhanoi.Button("BACK TO MENU").setSize(220,50).setPosition(215,500);
-    layer.appendChild(btn_backtomenu);
-
-    goog.events.listen(btn_backtomenu, ['mousedown', 'touchstart'], function(e){
-        towerofhanoi.play();
-        towerofhanoi.Game.resetMoviments();
-        towerofhanoi.loadMenu();
-        towerofhanoi.play_sound_initial();
-    });
-    
     
     goog.events.listen(btn_play, ['mousedown', 'touchstart'], function(e){
         towerofhanoi.play();
